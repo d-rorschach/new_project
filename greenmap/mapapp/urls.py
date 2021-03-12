d016ad5ce3ec7c4281101b39887914fd583080e6
d-rorschach <prayaspal777@gmail.com>
2021-03-12 15:45:47 +0530

    second commit

--- greenmap/mapapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('save_user_data/',views.save_user_data,name='save_user_data'),
-    path('c_offset/',views.c_offset,name='c_offset'),
-]
